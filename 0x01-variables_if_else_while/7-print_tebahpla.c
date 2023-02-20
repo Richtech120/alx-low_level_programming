0f665e1886aa93afd304b441a87b273071c15242
Richtech120 <richardfavout120@gmail.com>
2023-02-20 15:00:34 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Block of character
+ *Return: always 0 success
+ */
+
+int main(void)
+{
+	char c = 'z';
+for (c = 'z'; c >= 'a'; --c)
+putchar(c);
+putchar('\n');
+return (0);
+}
