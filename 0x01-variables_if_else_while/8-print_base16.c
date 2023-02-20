17886171ff04f800a87c40d3ae80bfd1edde7ff4
Richtech120 <richardfavout120@gmail.com>
2023-02-20 15:10:02 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Block of character
+ *Return: always 0 success
+ */
+
+int main(void)
+{
+	int c = 0;
+	{
+		for (c = 0; c <= 9; ++c)
+			putchar('0' + c);
+	}
+	{
+		for (c = 49; c <= 54; ++c)
+			putchar('0' + c);
+		printf("\n");
+	}
+return (0);
+}
