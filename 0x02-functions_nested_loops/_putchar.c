2dfc2e1a4b472a809e1db2467c206bc640ac49b0
Richtech120 <richardfavout120@gmail.com>
2023-02-23 14:35:07 -0800

    Task 0

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -6,6 +8,9 @@
  */
 
 int _putchar(char c)
+
 {
+
 	return (write(1, &c, 1));
+
 }
